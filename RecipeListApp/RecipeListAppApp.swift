8eff3507bdd2fbafff1a23ac2da568a149bb183b
surya sama <suryasama4@gmail.com>
2021-07-20 13:06:48 -0400

    Add TabView. its the starting point of this app

--- RecipeListApp/RecipeListAppApp.swift
+++ RecipeListApp/RecipeListAppApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct RecipeListAppApp: App {
     var body: some Scene {
         WindowGroup {
-            RecipeListView()
+      RecipeTabView()
+            
         }
     }
 }
